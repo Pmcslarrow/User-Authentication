b71c49c9528f7c353c988293ec748cab9d0ddea6
Paul McSlarrow <74205136+Pmcslarrow@users.noreply.github.com>
2022-06-19 21:46:38 -0700

    Update
    I was able to change it all to ES6. I also was able to learn how to connect my program to the firebase database rather than using a hard coded list...

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 // Imports
-const express = require('express');
-const app = express();
-const bcrypt = require('bcrypt')
+import express from 'express'
+import * as bcrypt from 'bcrypt'
+import alert from 'alert'
 const users = []
-let obj = null
+const app = express()
 let signedIn = false
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
+import { getDatabase, ref, set, onValue } from 'firebase/database'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwts7Rtk62UnacEv_j-0dhI5NNw8wCE9k",
+    authDomain: "user-authentication-75016.firebaseapp.com",
+    projectId: "user-authentication-75016",
+    storageBucket: "user-authentication-75016.appspot.com",
+    messagingSenderId: "513838819682",
+    appId: "1:513838819682:web:6a026d1fa86ab673b8a43b"
+  };
+  const application = initializeApp(firebaseConfig);
+  const fireStore = getFirestore(application)
+
+
+// Adds a new tree in the realtime database inside 'users'
+function writeUserData(userId, name, email, password) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId + '/'), {
+        name: name,
+        email: email,
+        password: password
+    })
+}
+
+function parseEmail( email ) {
+    let user2 = email.replace('@', '')
+    let userId = user2.replace('.', '')
+    return userId
+}
+
+
+/* When you try to access the homepage it checks if you are signed in first to allow access */
 app.get('/homepage', (req, res) => {
     if (signedIn)
     {
@@ -26,27 +61,32 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
-// When you press the login button
+/* When you press the login button */
 app.post('/login', async (req, resolve) => {
-    let loginEmail = req.body.email
-    let loginPassword = req.body.password
-    let i = 0;
-    //Iterate through the length of users. If the email ever matches then break
-    for (user of users)
-    {
-        if (user.email === loginEmail)
-        {
-            bcrypt.compare(loginPassword, users[i].password, function(err, res) {
-                if (res)
-                {
+    const db = getDatabase()
+    let attemptEmail = req.body.email
+    let attemptPassword = req.body.password
+    let userId = parseEmail(attemptEmail)
+    let userRef = ref(db, 'users/')
+    onValue(userRef, (snapshot) => {
+        const data = snapshot.val()
+        try {
+            let account = data[userId]
+            let hashedPassword = account.password
+            bcrypt.compare(attemptPassword, hashedPassword, function (err, res) {
+                if (res) {
                     signedIn = true
                     resolve.redirect('/homepage')
+                } else {
+                    resolve.redirect('/login')
+                    alert("Email or Password was incorrect")
                 }
+                
             })
-            break
+        } catch {
+            console.log("bcrypt error [-] ")
         }
-        i++
-    }
+    })
 
 })
 
@@ -54,25 +94,18 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-// When you press the register button
+/* When you press the register button */
 app.post('/register', async (req, res) => {
-
-    let email = req.body.email
-    let password = req.body.password
 
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
+        let user = req.body.email
+        let userId = parseEmail(user)
+        writeUserData(userId, req.body.name, req.body.email, hashedPassword)
         res.redirect('/login')
     } catch {
         res.redirect('/register')
     }
-    console.log(users)
 })
 
 app.listen(3000)
