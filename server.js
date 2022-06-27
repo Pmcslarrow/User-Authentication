295403668108c6f98ead4a8773e0c2f0723eb0cf
Paul McSlarrow <74205136+Pmcslarrow@users.noreply.github.com>
2022-06-26 19:08:58 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 let signedIn = false
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+app.use('/public', express.static('public'))
+
 
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
